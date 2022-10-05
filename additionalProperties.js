0c96b9ad00ac84273d580526ee5a936786e835e5
alltheraz <115084243+alltheraz@users.noreply.github.com>
2022-10-05 07:41:42 -0500

    Add files via upload

---
+++ additionalProperties.js
@@ -0,0 +1,7 @@
+Hooks.on('ready', () => {
+CONFIG.DND5E.weaponProperties['loud'] = 'Loud';
+CONFIG.DND5E.weaponProperties['misfire'] = 'Misfire';
+CONFIG.DND5E.weaponProperties['muzzle-loading'] = 'Muzzle-Loading';
+CONFIG.DND5E.weaponProperties['rifled'] = 'Rifled';
+CONFIG.DND5E.weaponProperties['scatter'] = 'Scatter';
+});
